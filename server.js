f2c8014b5b242760eba6212941ef1b9d6236fec9
Rachel Diaz <Rachel7113@gmail.com>
2021-03-13 15:26:37 -0500

    Notes can save & delete

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-let db = require('./db/db.json');
 
 //sets up the Express app
 const app = express();
@@ -10,21 +9,38 @@ const PORT = process.env.PORT || 3001;
 //sets up the express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//sets up 
+//set path to assets
 app.use("/assets", express.static('assets'));
 
 //setting up route
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')))
 
+let notes = [];
+let count = 0;
 
-app.route('/api/notes')
-    .get((req, res) => res.json(db))
-    .post((req, res) => {
-        console.log(req.body)
+app.get('/api/notes', (req, res) => res.json(notes))
+app.post('/api/notes', (req, res) => {
+    console.log(req.body)
+    let note = req.body
+    count++
+    note.id = count
+    notes.push(note);
+    console.log(notes)
+    res.json(notes);
+})
 
+app.delete('/api/notes/:id', (req, res) => {
+    let selected = req.params.id;
 
-    })
+    for (const note of notes) {
+
+        if (parseInt(selected) === note.id) {
+            notes.splice(notes.indexOf(note), 1)
+            res.json(notes)
+        }
+    }
+})
 
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
